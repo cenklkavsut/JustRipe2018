abf02ff3cf6513acaf3be3cde2247de3e0869b61
Kavshot <cenkkavsut@hotmail.com>
2018-11-09 15:13:43 +0000

    1572556,1553265,1706224 database added!

---
+++ dbo.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Customer]
+(
+[CustomerID] int NOT NULL PRIMARY KEY IDENTITY,
+[First Name] varchar(50) NOT NULL,
+[Surname] varchar(50) NOT NULL,
+[Contact Number] varchar(50) NOT NULL,
+[Email] varchar(50) NOT NULL,
+[OrderID] int NOT NULL,
+CONSTRAINT [FK_Customer_Orders] FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+)
+
