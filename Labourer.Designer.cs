c827248fe9be6ee9bbe5759da21950d1633180c6
PDRQ <PDRQ@users.noreply.github.com>
2018-11-13 21:09:49 +0000

    1708431 removed unused content (report), Timetable added to labourer cs page

--- Labourer.Designer.cs
+++ Labourer.Designer.cs
@@ -32,6 +32,19 @@
             this.tabLabourer = new System.Windows.Forms.TabControl();
             this.tabTimetableLabourer = new System.Windows.Forms.TabPage();
             this.btnLogoutLabourer = new System.Windows.Forms.Button();
+            this.friTextTt = new System.Windows.Forms.RichTextBox();
+            this.wedTextTt = new System.Windows.Forms.RichTextBox();
+            this.thurTextTt = new System.Windows.Forms.RichTextBox();
+            this.tueTextTt = new System.Windows.Forms.RichTextBox();
+            this.monTextTt = new System.Windows.Forms.RichTextBox();
+            this.DailyTasksLab = new System.Windows.Forms.TextBox();
+            this.DailyTasksRtb = new System.Windows.Forms.RichTextBox();
+            this.friLabTt = new System.Windows.Forms.TextBox();
+            this.thurLabTt = new System.Windows.Forms.TextBox();
+            this.wedLabTt = new System.Windows.Forms.TextBox();
+            this.tueLabTt = new System.Windows.Forms.TextBox();
+            this.monLabTt = new System.Windows.Forms.TextBox();
+            this.dateTimePickerLab = new System.Windows.Forms.DateTimePicker();
             this.tabLabourer.SuspendLayout();
             this.tabTimetableLabourer.SuspendLayout();
             this.SuspendLayout();
@@ -41,21 +54,32 @@
             this.tabLabourer.Controls.Add(this.tabTimetableLabourer);
             this.tabLabourer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabLabourer.Location = new System.Drawing.Point(0, 0);
-            this.tabLabourer.Margin = new System.Windows.Forms.Padding(6);
             this.tabLabourer.Name = "tabLabourer";
             this.tabLabourer.SelectedIndex = 0;
-            this.tabLabourer.Size = new System.Drawing.Size(1820, 1385);
+            this.tabLabourer.Size = new System.Drawing.Size(910, 720);
             this.tabLabourer.TabIndex = 0;
             // 
             // tabTimetableLabourer
             // 
             this.tabTimetableLabourer.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tabTimetableLabourer.BackgroundImage")));
+            this.tabTimetableLabourer.Controls.Add(this.friTextTt);
+            this.tabTimetableLabourer.Controls.Add(this.wedTextTt);
+            this.tabTimetableLabourer.Controls.Add(this.thurTextTt);
+            this.tabTimetableLabourer.Controls.Add(this.tueTextTt);
+            this.tabTimetableLabourer.Controls.Add(this.monTextTt);
+            this.tabTimetableLabourer.Controls.Add(this.DailyTasksLab);
+            this.tabTimetableLabourer.Controls.Add(this.DailyTasksRtb);
+            this.tabTimetableLabourer.Controls.Add(this.friLabTt);
+            this.tabTimetableLabourer.Controls.Add(this.thurLabTt);
+            this.tabTimetableLabourer.Controls.Add(this.wedLabTt);
+            this.tabTimetableLabourer.Controls.Add(this.tueLabTt);
+            this.tabTimetableLabourer.Controls.Add(this.monLabTt);
+            this.tabTimetableLabourer.Controls.Add(this.dateTimePickerLab);
             this.tabTimetableLabourer.Controls.Add(this.btnLogoutLabourer);
-            this.tabTimetableLabourer.Location = new System.Drawing.Point(8, 51);
-            this.tabTimetableLabourer.Margin = new System.Windows.Forms.Padding(6);
+            this.tabTimetableLabourer.Location = new System.Drawing.Point(4, 29);
             this.tabTimetableLabourer.Name = "tabTimetableLabourer";
-            this.tabTimetableLabourer.Padding = new System.Windows.Forms.Padding(6);
-            this.tabTimetableLabourer.Size = new System.Drawing.Size(1804, 1326);
+            this.tabTimetableLabourer.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabTimetableLabourer.Size = new System.Drawing.Size(902, 687);
             this.tabTimetableLabourer.TabIndex = 0;
             this.tabTimetableLabourer.Text = "Timetable";
             this.tabTimetableLabourer.UseVisualStyleBackColor = true;
@@ -63,33 +87,135 @@
             // btnLogoutLabourer
             // 
             this.btnLogoutLabourer.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.btnLogoutLabourer.Location = new System.Drawing.Point(1565, 31);
-            this.btnLogoutLabourer.Margin = new System.Windows.Forms.Padding(6);
+            this.btnLogoutLabourer.Location = new System.Drawing.Point(782, 16);
             this.btnLogoutLabourer.Name = "btnLogoutLabourer";
-            this.btnLogoutLabourer.Size = new System.Drawing.Size(178, 56);
+            this.btnLogoutLabourer.Size = new System.Drawing.Size(89, 29);
             this.btnLogoutLabourer.TabIndex = 7;
             this.btnLogoutLabourer.TabStop = false;
             this.btnLogoutLabourer.Text = "LOGOUT";
             this.btnLogoutLabourer.UseVisualStyleBackColor = false;
             this.btnLogoutLabourer.Click += new System.EventHandler(this.btnLogoutLabourer_Click);
             // 
+            // friTextTt
+            // 
+            this.friTextTt.Location = new System.Drawing.Point(629, 145);
+            this.friTextTt.Name = "friTextTt";
+            this.friTextTt.Size = new System.Drawing.Size(100, 253);
+            this.friTextTt.TabIndex = 38;
+            this.friTextTt.Text = "";
+            // 
+            // wedTextTt
+            // 
+            this.wedTextTt.Location = new System.Drawing.Point(417, 145);
+            this.wedTextTt.Name = "wedTextTt";
+            this.wedTextTt.Size = new System.Drawing.Size(100, 253);
+            this.wedTextTt.TabIndex = 36;
+            this.wedTextTt.Text = "";
+            // 
+            // thurTextTt
+            // 
+            this.thurTextTt.Location = new System.Drawing.Point(523, 145);
+            this.thurTextTt.Name = "thurTextTt";
+            this.thurTextTt.Size = new System.Drawing.Size(100, 253);
+            this.thurTextTt.TabIndex = 37;
+            this.thurTextTt.Text = "";
+            // 
+            // tueTextTt
+            // 
+            this.tueTextTt.Location = new System.Drawing.Point(311, 145);
+            this.tueTextTt.Name = "tueTextTt";
+            this.tueTextTt.Size = new System.Drawing.Size(100, 253);
+            this.tueTextTt.TabIndex = 35;
+            this.tueTextTt.Text = "";
+            // 
+            // monTextTt
+            // 
+            this.monTextTt.Location = new System.Drawing.Point(205, 145);
+            this.monTextTt.Name = "monTextTt";
+            this.monTextTt.Size = new System.Drawing.Size(100, 253);
+            this.monTextTt.TabIndex = 34;
+            this.monTextTt.Text = "";
+            // 
+            // DailyTasksLab
+            // 
+            this.DailyTasksLab.Location = new System.Drawing.Point(34, 70);
+            this.DailyTasksLab.Name = "DailyTasksLab";
+            this.DailyTasksLab.Size = new System.Drawing.Size(165, 26);
+            this.DailyTasksLab.TabIndex = 33;
+            this.DailyTasksLab.Text = "Daily Tasks";
+            // 
+            // DailyTasksRtb
+            // 
+            this.DailyTasksRtb.Location = new System.Drawing.Point(34, 102);
+            this.DailyTasksRtb.Name = "DailyTasksRtb";
+            this.DailyTasksRtb.Size = new System.Drawing.Size(165, 409);
+            this.DailyTasksRtb.TabIndex = 32;
+            this.DailyTasksRtb.Text = "";
+            // 
+            // friLabTt
+            // 
+            this.friLabTt.Location = new System.Drawing.Point(629, 113);
+            this.friLabTt.Name = "friLabTt";
+            this.friLabTt.Size = new System.Drawing.Size(100, 26);
+            this.friLabTt.TabIndex = 31;
+            this.friLabTt.Text = "Friday";
+            // 
+            // thurLabTt
+            // 
+            this.thurLabTt.Location = new System.Drawing.Point(523, 113);
+            this.thurLabTt.Name = "thurLabTt";
+            this.thurLabTt.Size = new System.Drawing.Size(100, 26);
+            this.thurLabTt.TabIndex = 30;
+            this.thurLabTt.Text = "Thursday";
+            // 
+            // wedLabTt
+            // 
+            this.wedLabTt.Location = new System.Drawing.Point(417, 113);
+            this.wedLabTt.Name = "wedLabTt";
+            this.wedLabTt.Size = new System.Drawing.Size(100, 26);
+            this.wedLabTt.TabIndex = 29;
+            this.wedLabTt.Text = "Wednesday";
+            // 
+            // tueLabTt
+            // 
+            this.tueLabTt.Location = new System.Drawing.Point(311, 113);
+            this.tueLabTt.Name = "tueLabTt";
+            this.tueLabTt.Size = new System.Drawing.Size(100, 26);
+            this.tueLabTt.TabIndex = 28;
+            this.tueLabTt.Text = "Tuesday";
+            // 
+            // monLabTt
+            // 
+            this.monLabTt.Location = new System.Drawing.Point(205, 113);
+            this.monLabTt.Name = "monLabTt";
+            this.monLabTt.Size = new System.Drawing.Size(100, 26);
+            this.monLabTt.TabIndex = 27;
+            this.monLabTt.Text = "Monday";
+            // 
+            // dateTimePickerLab
+            // 
+            this.dateTimePickerLab.Location = new System.Drawing.Point(539, 19);
+            this.dateTimePickerLab.Name = "dateTimePickerLab";
+            this.dateTimePickerLab.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePickerLab.TabIndex = 26;
+            // 
             // Labourer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Tan;
-            this.ClientSize = new System.Drawing.Size(1800, 1310);
+            this.ClientSize = new System.Drawing.Size(900, 552);
             this.Controls.Add(this.tabLabourer);
             this.Cursor = System.Windows.Forms.Cursors.Default;
             this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(6);
             this.MaximizeBox = false;
             this.Name = "Labourer";
             this.Text = "JustRipe2018 - Labourer";
             this.Load += new System.EventHandler(this.Labourer_Load);
             this.tabLabourer.ResumeLayout(false);
             this.tabTimetableLabourer.ResumeLayout(false);
+            this.tabTimetableLabourer.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -99,5 +225,18 @@
         private System.Windows.Forms.TabControl tabLabourer;
         private System.Windows.Forms.TabPage tabTimetableLabourer;
         private System.Windows.Forms.Button btnLogoutLabourer;
+        private System.Windows.Forms.RichTextBox friTextTt;
+        private System.Windows.Forms.RichTextBox wedTextTt;
+        private System.Windows.Forms.RichTextBox thurTextTt;
+        private System.Windows.Forms.RichTextBox tueTextTt;
+        private System.Windows.Forms.RichTextBox monTextTt;
+        private System.Windows.Forms.TextBox DailyTasksLab;
+        private System.Windows.Forms.RichTextBox DailyTasksRtb;
+        private System.Windows.Forms.TextBox friLabTt;
+        private System.Windows.Forms.TextBox thurLabTt;
+        private System.Windows.Forms.TextBox wedLabTt;
+        private System.Windows.Forms.TextBox tueLabTt;
+        private System.Windows.Forms.TextBox monLabTt;
+        private System.Windows.Forms.DateTimePicker dateTimePickerLab;
     }
 }
--- Manager.Designer.cs
+++ Manager.Designer.cs
@@ -32,13 +32,6 @@
             this.tabManager = new System.Windows.Forms.TabControl();
             this.tabReport = new System.Windows.Forms.TabPage();
             this.btnLogout = new System.Windows.Forms.Button();
-            this.tabReportOpt = new System.Windows.Forms.TabControl();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.tabPage5 = new System.Windows.Forms.TabPage();
-            this.tabPage6 = new System.Windows.Forms.TabPage();
-            this.tabPage7 = new System.Windows.Forms.TabPage();
-            this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -154,9 +147,17 @@
             this.btnAddBuyers = new System.Windows.Forms.Button();
             this.btnViewBuyers = new System.Windows.Forms.Button();
             this.btnViewStock = new System.Windows.Forms.Button();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabReportOpt = new System.Windows.Forms.TabControl();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.dataGridView5 = new System.Windows.Forms.DataGridView();
+            this.dataGridView6 = new System.Windows.Forms.DataGridView();
             this.tabManager.SuspendLayout();
             this.tabReport.SuspendLayout();
-            this.tabReportOpt.SuspendLayout();
             this.tabTimeTable.SuspendLayout();
             this.tabJob.SuspendLayout();
             this.formManageJob.SuspendLayout();
@@ -173,6 +174,15 @@
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridAddStore)).BeginInit();
             this.tabPage2.SuspendLayout();
+            this.tabPage6.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabReportOpt.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).BeginInit();
             this.SuspendLayout();
             // 
             // tabManager
@@ -199,7 +209,6 @@
             this.tabReport.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tabReport.BackgroundImage")));
             this.tabReport.Controls.Add(this.btnLogout);
             this.tabReport.Controls.Add(this.tabReportOpt);
-            this.tabReport.Controls.Add(this.button5);
             this.tabReport.Controls.Add(this.button4);
             this.tabReport.Controls.Add(this.button3);
             this.tabReport.Controls.Add(this.button2);
@@ -224,80 +233,6 @@
             this.btnLogout.Text = "LOGOUT";
             this.btnLogout.UseVisualStyleBackColor = false;
             this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
-            // 
-            // tabReportOpt
-            // 
-            this.tabReportOpt.Controls.Add(this.tabPage3);
-            this.tabReportOpt.Controls.Add(this.tabPage4);
-            this.tabReportOpt.Controls.Add(this.tabPage5);
-            this.tabReportOpt.Controls.Add(this.tabPage6);
-            this.tabReportOpt.Controls.Add(this.tabPage7);
-            this.tabReportOpt.Location = new System.Drawing.Point(178, 69);
-            this.tabReportOpt.Name = "tabReportOpt";
-            this.tabReportOpt.SelectedIndex = 0;
-            this.tabReportOpt.Size = new System.Drawing.Size(625, 340);
-            this.tabReportOpt.TabIndex = 5;
-            // 
-            // tabPage3
-            // 
-            this.tabPage3.BackColor = System.Drawing.Color.Tan;
-            this.tabPage3.Location = new System.Drawing.Point(4, 29);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(617, 307);
-            this.tabPage3.TabIndex = 0;
-            this.tabPage3.Text = "tabPage3";
-            // 
-            // tabPage4
-            // 
-            this.tabPage4.Location = new System.Drawing.Point(4, 29);
-            this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(617, 307);
-            this.tabPage4.TabIndex = 1;
-            this.tabPage4.Text = "tabPage4";
-            this.tabPage4.UseVisualStyleBackColor = true;
-            // 
-            // tabPage5
-            // 
-            this.tabPage5.Location = new System.Drawing.Point(4, 29);
-            this.tabPage5.Name = "tabPage5";
-            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage5.Size = new System.Drawing.Size(617, 307);
-            this.tabPage5.TabIndex = 2;
-            this.tabPage5.Text = "tabPage5";
-            this.tabPage5.UseVisualStyleBackColor = true;
-            // 
-            // tabPage6
-            // 
-            this.tabPage6.Location = new System.Drawing.Point(4, 29);
-            this.tabPage6.Name = "tabPage6";
-            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage6.Size = new System.Drawing.Size(617, 307);
-            this.tabPage6.TabIndex = 3;
-            this.tabPage6.Text = "tabPage6";
-            this.tabPage6.UseVisualStyleBackColor = true;
-            // 
-            // tabPage7
-            // 
-            this.tabPage7.Location = new System.Drawing.Point(4, 29);
-            this.tabPage7.Name = "tabPage7";
-            this.tabPage7.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage7.Size = new System.Drawing.Size(617, 307);
-            this.tabPage7.TabIndex = 4;
-            this.tabPage7.Text = "tabPage7";
-            this.tabPage7.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button5.Location = new System.Drawing.Point(33, 333);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(100, 60);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Report 5";
-            this.button5.UseVisualStyleBackColor = false;
-            this.button5.Click += new System.EventHandler(this.btnRep5_Click);
             // 
             // button4
             // 
@@ -1598,6 +1533,98 @@
             this.btnViewStock.UseVisualStyleBackColor = false;
             this.btnViewStock.Click += new System.EventHandler(this.btnViewStock_Click_1);
             // 
+            // tabPage6
+            // 
+            this.tabPage6.BackColor = System.Drawing.Color.Tan;
+            this.tabPage6.Controls.Add(this.dataGridView6);
+            this.tabPage6.Location = new System.Drawing.Point(4, 29);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(617, 307);
+            this.tabPage6.TabIndex = 3;
+            this.tabPage6.Text = "Vehicles&Labour";
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.BackColor = System.Drawing.Color.Tan;
+            this.tabPage5.Controls.Add(this.dataGridView5);
+            this.tabPage5.Location = new System.Drawing.Point(4, 29);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(617, 307);
+            this.tabPage5.TabIndex = 2;
+            this.tabPage5.Text = "Storage";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.BackColor = System.Drawing.Color.Tan;
+            this.tabPage4.Controls.Add(this.dataGridView4);
+            this.tabPage4.Location = new System.Drawing.Point(4, 29);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(617, 307);
+            this.tabPage4.TabIndex = 1;
+            this.tabPage4.Text = "Fertilizer";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.Tan;
+            this.tabPage3.Controls.Add(this.dataGridView1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 29);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(617, 307);
+            this.tabPage3.TabIndex = 0;
+            this.tabPage3.Text = "Crops&Cultivation";
+            // 
+            // tabReportOpt
+            // 
+            this.tabReportOpt.Controls.Add(this.tabPage3);
+            this.tabReportOpt.Controls.Add(this.tabPage4);
+            this.tabReportOpt.Controls.Add(this.tabPage5);
+            this.tabReportOpt.Controls.Add(this.tabPage6);
+            this.tabReportOpt.Location = new System.Drawing.Point(178, 69);
+            this.tabReportOpt.Name = "tabReportOpt";
+            this.tabReportOpt.SelectedIndex = 0;
+            this.tabReportOpt.Size = new System.Drawing.Size(625, 340);
+            this.tabReportOpt.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Tan;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-4, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(625, 311);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.BackgroundColor = System.Drawing.Color.Tan;
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.Size = new System.Drawing.Size(621, 311);
+            this.dataGridView4.TabIndex = 0;
+            // 
+            // dataGridView5
+            // 
+            this.dataGridView5.BackgroundColor = System.Drawing.Color.Tan;
+            this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView5.Location = new System.Drawing.Point(-4, 0);
+            this.dataGridView5.Name = "dataGridView5";
+            this.dataGridView5.Size = new System.Drawing.Size(625, 311);
+            this.dataGridView5.TabIndex = 0;
+            // 
+            // dataGridView6
+            // 
+            this.dataGridView6.BackgroundColor = System.Drawing.Color.Tan;
+            this.dataGridView6.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView6.Location = new System.Drawing.Point(-4, 0);
+            this.dataGridView6.Name = "dataGridView6";
+            this.dataGridView6.Size = new System.Drawing.Size(625, 311);
+            this.dataGridView6.TabIndex = 0;
+            // 
             // Manager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -1615,7 +1642,6 @@
             this.Load += new System.EventHandler(this.Manager_Load);
             this.tabManager.ResumeLayout(false);
             this.tabReport.ResumeLayout(false);
-            this.tabReportOpt.ResumeLayout(false);
             this.tabTimeTable.ResumeLayout(false);
             this.tabTimeTable.PerformLayout();
             this.tabJob.ResumeLayout(false);
@@ -1638,6 +1664,15 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridAddStore)).EndInit();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
+            this.tabPage6.ResumeLayout(false);
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabReportOpt.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -1739,13 +1774,6 @@
         private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.TextBox textBox15;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.TabControl tabReportOpt;
-        private System.Windows.Forms.TabPage tabPage3;
-        private System.Windows.Forms.TabPage tabPage4;
-        private System.Windows.Forms.TabPage tabPage5;
-        private System.Windows.Forms.TabPage tabPage6;
-        private System.Windows.Forms.TabPage tabPage7;
-        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
@@ -1769,5 +1797,14 @@
         private System.Windows.Forms.Label label15;
         private System.Windows.Forms.ComboBox editJobType;
         private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.TabControl tabReportOpt;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.DataGridView dataGridView5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.DataGridView dataGridView6;
     }
 }
